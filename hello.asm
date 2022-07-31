ab0c800bf179e2424092b389055080688899604c
sigh <sigh.projects@gmail.com>
2022-07-31 22:24:52 +1000

    Only store odd elements in prime_array.

--- hello.asm
+++ hello.asm
@@ -1,8 +1,9 @@
 ; Run with:
 ;   nasm -fmacho64 hello.asm && gcc hello.o && ./a.out
 
-MAX_P   equ 100_000_000
+MAX_P        equ 100_000_000
 ; MAX_P   equ 100
+ARRAY_SIZE   equ MAX_P/2 + 1
 
 NEWLINE     equ 10 ; newline ascii character
 
@@ -14,23 +15,27 @@ section   .text
 _main:
   push      rsp                     ; Required for alignment
 
+  mov       r12, 2                  ; p = 2
+  call      print_r12
+  mov       r14, 0                  ; x = 0
   mov       r12, 1                  ; p = 1
   lea       r13, [rel prime_array]  ; q = prime_array
 
-  mov       rax, 2
+  mov       rax, 1
 set_prime_array:
   mov       [r13+rax], byte 1       ; q[a++] = 1
   inc       rax
-  cmp       rax, MAX_P
-  jle       set_prime_array
+  cmp       rax, ARRAY_SIZE
+  jl        set_prime_array
 
 find_next_p:
   xor       eax, eax                ; Must clear because we only read bytes into al
 find_next_p_loop:
-  inc       r12                     ; p++;
+  inc       r14                     ; x++
+  add       r12, 2                  ; p += 2
   cmp       r12, MAX_P              ; if (p > MAX_P) exit
   jg        exit
-  mov       al, [r13+r12]           ; if (q[p] == 0) find_next_p
+  mov       al, [r13+r14]           ; if (q[x] == 0) find_next_p
   test      eax, eax
   jz        find_next_p_loop
 
@@ -38,10 +43,11 @@ find_next_p_loop:
 
   mov       rax, r12                ; a = p*p
   mul       rax
+  shr       rax, 1
 
 clear_prime_multiples:
-  cmp       rax, MAX_P              ; if (a > MAX_P) continue
-  jg        find_next_p
+  cmp       rax, ARRAY_SIZE         ; if (a >= ARRAY_SIZE) find_next_p
+  jge       find_next_p
   mov       [r13+rax], byte 0       ; q[a] = 0
   add       rax, r12                ; a += p
   jmp       clear_prime_multiples
@@ -79,6 +85,5 @@ print_buffer_end:
 
 section .bss
 
-prime_array:
-  resb 1
-  resb MAX_P
+prime_array:                        ; prime_array has MAX_P+1 elements
+  resb ARRAY_SIZE

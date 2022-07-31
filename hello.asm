3f7cefcb8b3f4744514b0c4e93f9f7e02504a935
sigh <sigh.projects@gmail.com>
2022-07-31 12:13:29 +1000

    Simple is_prime loop.

--- hello.asm
+++ hello.asm
@@ -10,11 +10,33 @@ section   .text
 _main:
   push      rbx                     ; Call stack must be aligned
 
-  lea       rdi, [rel message]      ; First argument is address of message
-  call      _puts                   ; puts(message)
+  mov       r12, $47                 ; x = p
+  mov       r13d, $2                 ; i = 2
+  mov       r14, $1                  ; is_prime = true
+is_prime_loop:
+  lea       rdi, [rel format]       ; First argument: format string
+  mov       rsi, r13                 ; Second argument: number to formart
+  call      _printf
+
+  mov       eax, r13d
+  mul       eax                     ; a = i*i
+  cmp       rax, r12                ; if (a > p) done
+  jg        done
+  xor       rdx, rdx
+  mov       rax, r12
+  idiv      r13                     ; d = p % i
+  test      rdx, rdx
+  jz        not_prime               ; if (d == 0) goto not_prime
+  inc       r13d                    ; i++
+  jmp is_prime_loop
+
+not_prime:
+  mov       r14, $0
+
+done:
 
   lea       rdi, [rel format]       ; First argument: format string
-  mov       rsi, $1                 ; Second argument: number to formart
+  mov       rsi, r14                ; Second argument: number to formart
   call      _printf
 
   pop       rbx                     ; Fix up stack before returning
@@ -23,7 +45,5 @@ _main:
 
 section   .data
 
-message:
-  db        "Hello world", 0
 format:
-  db        "%d", 10, 0
+  db        "%d", 10, 0             ; Printf format string, followed by newline and null

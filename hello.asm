83fb382d50d137ae592b1d3b5ac8578fe325313f
sigh <sigh.projects@gmail.com>
2022-07-31 15:37:45 +1000

    Manually convert number to string.

--- hello.asm
+++ hello.asm
@@ -1,17 +1,20 @@
 ; Run with:
 ;   nasm -fmacho64 hello.asm && gcc hello.o && ./a.out
 
-MAX_P  equ 10_000_000
+MAX_P   equ 10_000_000
+; MAX_P   equ 10
+
+NEWLINE     equ 10 ; newline ascii character
 
 global    _main
-extern    _printf
+extern    _puts
 
 section   .text
 
 _main:
   push      rsp                     ; Required for alignment
 
-  mov r12, 2
+  mov       r12, 2
   call      print_r12
 
   mov       r12, 3                  ; p = 3
@@ -47,14 +50,26 @@ exit:
 
 ; Print out the number at r12.
 print_r12:
-  push rsp                          ; Required for alignment.
-  lea       rdi, [rel format]
-  mov       rsi, r12
-  call      _printf
-  pop rsp
+  push      rsp                     ; Required for alignment.
+  lea       rdi, [rel print_buffer_end] ; buf = print_buffer_end
+  mov       rax, r12                ; a = p
+  mov       rbx, 10                 ; b = 10
+print_r12_loop:
+  xor       rdx, rdx                ; clear d
+  idiv      rbx                     ; a, d = divmod(a, b)
+  lea       rdx, [rdx + 48]         ; d += 48 (convert digit to ascii)
+  dec       rdi                     ; buf--
+  mov       [rdi], dl               ; *buf = d (add char to buffer)
+  test      rax, rax                ; if a != 0: continue
+  jnz       print_r12_loop
+print_r12_finish:
+  call _puts
+  pop       rsp
   ret
 
 section   .data
 
-format:
-  db        "%d", 10, 0             ; Printf format string, followed by newline and null
+print_buffer:
+  times 20 db 0                     ; Enough space to store 2**64
+print_buffer_end:
+  db 0
